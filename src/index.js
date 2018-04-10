83a608ab63d5cbfdc0c56a4bba802cafa6935bb1
dnovac <danynovac@gmail.com>
2018-04-10 23:09:47 +0300

    #2nd init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
+import './css/style.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
